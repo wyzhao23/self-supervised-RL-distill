f98b1ff74c878595b4ab4eeef7065b4ac17f14dd
wyzhao030 <wyzhao030@gmail.com>
2022-09-06 20:56:37 -0700

    Add GAE

--- src/utils.py
+++ src/utils.py
@@ -307,3 +307,21 @@ def gae(rewards, values, episode_ends, gamma, lam):
         # And store it
         advantages[:, t] = gae_step
     return advantages
+
+
+def compute_GAE(self, mb_rewards, mb_values, mb_dones):
+    mb_rewards = np.asarray(mb_rewards, dtype=np.float32)
+    mb_values = np.asarray(mb_values, dtype=np.float32)
+    mb_dones = np.asarray(mb_dones, dtype=np.bool)
+    last_values = self.model.value(self.obs, S=self.states, M=self.dones)
+    mb_advs = np.zeros_like(mb_rewards)
+    lastgaelam = 0
+    for t in reversed(range(self.nsteps)):  # 倒序实现，便于进行递归
+        if t == self.nsteps - 1:  # 如果是最后一步，要判断当前是否是终止状态，如果是，next_value就是0
+            nextnonterminal = 1.0 - self.dones
+            nextvalues = last_values
+        else:
+            nextnonterminal = 1.0 - mb_dones[t + 1]
+            nextvalues = mb_values[t + 1]
+        delta = mb_rewards[t] + self.gamma * nextvalues * nextnonterminal - mb_values[t]
+        mb_advs[t] = lastgaelam = delta + self.gamma * self.lam * nextnonterminal * lastgaelam
